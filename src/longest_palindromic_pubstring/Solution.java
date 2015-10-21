aee8df0f62d40342b58a6e0d0c6cb5640073d15b
iceke <iceke1025@gmail.com>
2015-10-21 20:50:52 +0800

    continue

---
+++ src/longest_palindromic_pubstring/Solution.java
@@ -0,0 +1,18 @@
+package longest_palindromic_pubstring;
+
+/**
+ * Created by iceke on 15/10/21.
+ */
+public class Solution {
+    public String longestPalindrome(String s) {
+
+
+        return null;
+
+    }
+
+    public static void main(String args[]){
+
+    }
+
+}
