9acedd7e28d8a25f611469bc26b00aa63511ebf0
iceke <iceke1025@gmail.com>
2015-12-11 09:34:35 +0800

    go on

---
+++ src/container_with_most_water/Solution.java
@@ -0,0 +1,32 @@
+package container_with_most_water;
+
+/**
+ * Created by iceke on 15/12/9.
+ */
+public class Solution {
+
+    public static int maxArea(int[] height) {
+        int max = 0;
+
+        if(height == null || height.length<2){
+            return 0;
+        }
+        int left = 0;
+        int right = height.length-1;
+        while(left<right){
+            max = Math.max(max,(right-left)*Math.min(height[left],height[right]));
+            if(height[left]<height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return max;
+    }
+
+
+    public static void main(String args[]){
+        int[] height = {4,1,2,3};
+        System.out.println(maxArea(height));
+    }
+}
---
+++ src/longest_common_prefix/Solution.java
@@ -0,0 +1,32 @@
+package longest_common_prefix;
+
+/**
+ * Created by iceke on 15/12/10.
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ */
+public class Solution {
+    public static String longestCommonPrefix(String[] strs) {
+        if(strs == null || strs.length == 0){
+            return "";
+        }
+        for (int j = 0; j < strs[0].length();j++){
+            char temp = strs[0].charAt(j);
+            for(int i=0;i <strs.length;i++){
+                if(j == strs[i].length() || strs[i].charAt(j)!=temp){
+                    return strs[0].substring(0,j);
+                }
+
+            }
+        }
+
+
+        return strs[0];
+
+    }
+
+    public static void main(String args[]){
+        String[] strs = {"sdaf","sda","sda"};
+        System.out.println(longestCommonPrefix(strs));
+
+    }
+}
